a5ec7ca97909b01d311c5816a139629b82900d81
vadimor <vadimorbiluy@gmail.com>
2022-01-03 14:27:42 +0200

    ServiceAbstract

---
+++ Module3HW7/Service/Abstract/IConfigService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Module3HW7.Models;
+
+namespace Module3HW7.Service.Abstract
+{
+    public interface IConfigService
+    {
+        public Task<Config> GetConfigAsync();
+    }
+}
---
+++ Module3HW7/Service/Abstract/IFileService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Module3HW7.Service.Abstract
+{
+    public interface IFileService : IDisposable
+    {
+        public Task WriteStreamAsync(string path, string text);
+        public Task<string> ReadStreamAsync(string path);
+        public Task<string> ReadAsync(string path);
+        public Task WriteAsync(string path, string text);
+    }
+}
