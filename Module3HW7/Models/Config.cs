51f40440c87ae2ff2e68d40e710bf9ea181e7cc0
vadimor <vadimorbiluy@gmail.com>
2022-01-03 14:27:24 +0200

    Models

---
+++ Module3HW7/Models/Config.cs
@@ -0,0 +1,8 @@
+﻿namespace Module3HW7.Models
+{
+    public class Config
+    {
+        public BackupConfig BackupConfig { get; set; }
+        public LogConfig LogConfig { get; set; }
+    }
+}
---
+++ Module3HW7/Models/LogConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace Module3HW7.Models
+{
+    public class LogConfig
+    {
+        public int BackupStep { get; set; }
+        public string DirectoryLog { get; set; }
+        public string TimeFormatFileName { get; set; }
+    }
+}
